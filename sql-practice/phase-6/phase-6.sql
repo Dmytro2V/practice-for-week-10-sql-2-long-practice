fc53209823a5507725ec9536b3d172149a694e45
Dmytro2V <op2vin@gmail.com>
2023-03-06 23:22:48 +0200

    done all: phase1, phase2, phase3, phase4, phase5, phase6

--- sql-practice/phase-6/phase-6.sql
+++ sql-practice/phase-6/phase-6.sql
@@ -1,2 +1,19 @@
 -- Update the toy with the name of "Cheetos" to have a name of "Pooky"
 -- Your code here
+.print ------------ before
+SELECT * FROM cats;
+.print .
+SELECT * FROM toys;
+UPDATE toys 
+    SET name = 'Pooky'
+    WHERE toys.name = 'Cheetos' 
+    AND cat_id = (
+        SELECT id FROM cats 
+            WHERE name = 'Garfield'
+            LIMIT 1
+        )
+;
+.print ------- updated
+SELECT * FROM cats;
+.print .
+SELECT * FROM toys;
